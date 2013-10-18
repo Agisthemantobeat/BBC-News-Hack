d7cddc4ebbcfe341050e2f7f350558e740457d73
apoclyps <kyle90adam@hotmail.com>
2013-10-18 03:05:58 +0100

    Minor tweaks

--- D3.js Node Graph/generateJson.php
+++ D3.js Node Graph/generateJson.php
@@ -2,15 +2,25 @@
 
 $nodes[5];
 $links[4];
+$value =10;
+$entity = "story";
 
 for($i=0;$i<5;$i++){
 	$currentStory = $i +1;
+
+
+	if($entity=="story"){
+		$entity="event";
+	}else{
+		$entity="story";
+	}
+
 	$temparray = array(
             "name" => "Story $currentStory",
             "full_name" => "Story $currentStory",
            	"type" => 1,
            	"slug" => "www.bbc.co.uk",
-           	"entity" => "story",
+           	"entity" => $entity,
            	"img_hrefD" => "",
            	"img_hrefL" => "" 
            	);
@@ -19,10 +29,17 @@ $nodes[$i] = $temparray;
 }
 
 for($j=0;$j<4;$j++){
+	if($value==10){
+		$value=1;
+	}
+	else{
+		$value=10;
+	}
+
 	$temparray = array(
             "source" => $j,
             "target" => $j+1,
-            "value" => 1,
+            "value" => $value,
             "distance" => 5
         );
 
